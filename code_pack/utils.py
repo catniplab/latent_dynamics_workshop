fffdfa55c0cc7c381baed9268a71a7945ebda908
matthew-dowling <matthew.dowling@stonybrook.edu>
2022-10-08 15:08:29 -0400

    ell vlgp, gpfa, and pca

--- code_pack/utils.py
+++ code_pack/utils.py
@@ -1,11 +1,41 @@
 import torch
 
-def expected_ll_poisson(Y, m, P, C, delta):
-    log_rate = C(m) + 0.5 * torch.einsum('nl, btlk, kn -> btn', C.weight, P, C.weight)
+
+def expected_ll_poisson(Y, m, P, C, delta, dtype=torch.float32):
+    m_t = torch.tensor(m, dtype=dtype)
+    P_t = torch.tensor(P, dtype=dtype)
+    Y_t = torch.tensor(Y, dtype=dtype)
+
+    log_rate = C(m_t) + 0.5 * torch.einsum('nl, btl, nl -> btn', C.weight, P_t, C.weight)
     likelihood_pdf = torch.distributions.Poisson(delta * torch.exp(log_rate))
     likelihood_pdf = torch.distributions.Independent(likelihood_pdf, 2)
-    log_prob = likelihood_pdf.log_prob(Y)
+    log_prob = likelihood_pdf.log_prob(Y_t)
 
-    return log_prob
+    return torch.mean(log_prob)
 
 
+def estimate_readout_matrix(Y, m, P, delta, n_iter=2500):
+    n_trials = Y.shape[0]
+    n_neuron = Y.shape[2]
+    n_latent = m.shape[2]
+    n_time_bins = Y.shape[1]
+    M = torch.zeros((n_trials, n_time_bins, n_latent))
+    C_hat = torch.nn.Linear(n_latent, n_neuron, bias=True)
+
+    for n in range(n_trials):
+        M[n] = torch.tensor(m[n])
+
+    opt = torch.optim.Adam(C_hat.parameters(), lr=1e-2)
+    loss_log = []
+
+    for i in range(n_iter):
+        log_r = C_hat(M)
+        ell = torch.tensor(Y) * log_r - delta * torch.exp(log_r)
+        loss = -1 * torch.sum(ell)
+
+        loss.backward()
+        opt.step()
+        opt.zero_grad()
+        loss_log.append(loss.item())
+
+    return C_hat
