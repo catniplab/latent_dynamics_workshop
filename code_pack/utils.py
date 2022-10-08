fb338f1048796704b42f2082902aacf1f7b4deed
Tushar Arora <arora.tushar1208@gmail.com>
2022-10-08 14:50:51 -0400

    utils update; adding vjf and vlgp to env

--- code_pack/utils.py
+++ code_pack/utils.py
@@ -1,4 +1,6 @@
 import torch
+import numpy as np
+from einops import rearrange
 
 def expected_ll_poisson(Y, m, P, C, delta):
     log_rate = C(m) + 0.5 * torch.einsum('nl, btlk, kn -> btn', C.weight, P, C.weight)
@@ -7,5 +9,11 @@ def expected_ll_poisson(Y, m, P, C, delta):
     log_prob = likelihood_pdf.log_prob(Y)
 
     return log_prob
+def best_fit_transformation(X, X_lat, n_trials, n_time_bins, n_latents):
+    # regress to account for invariance
+    X_hat = rearrange(np.stack(X_lat, 0), 'trials lat time -> (trials time) lat')
+    S = np.linalg.pinv(X_hat) @ X.reshape(n_trials * n_time_bins, n_latents)
+    X_hat_tilde = X_hat @ S
+    X_hat_tilde = X_hat_tilde.reshape(n_trials, n_time_bins, n_latents)
 
-
+    return X_hat_tilde
